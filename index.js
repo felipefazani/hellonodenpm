97609814b6ae93fe0163d80e94678feef6cdd00b
unknown <180255@bentoquirino.edu>
2020-03-02 15:40:08 -0300

    adicionando nomes aleatórios

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const http = require('http');
 
+const { Form } = require('brazil-mocker');
+
+let nome = Form.names.firstName.generate();
+let sobren = Form.names.lastName.generate();
+
+
 http.createServer((req,res) => {
 	res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-	res.end('Hello World, NPM!');
+	res.end('Oi, ' + nome + ' ' + sobren);
 }).listen(3000);
 
 console.log('O node esta rodando na porta 3000, para encerrar o serviço pressione Ctrl + C');
