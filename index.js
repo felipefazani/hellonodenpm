0d8affb2ccbd0f7686aa69837303f6cddc511a74
unknown <180255@bentoquirino.edu>
2020-03-02 14:49:32 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+http.createServer((req,res) => {
+	res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+	res.end('Hello World, NPM!');
+}).listen(3000);
+
+console.log('O node esta rodando na porta 3000, para encerrar o serviço pressione Ctrl + C');
